642ec1a40192bc98cab489baf7be2dfe949daea4
Krystian Kuczek <krystian@kuczek.eu>
2013-04-15 09:53:12 +0200

    Structure fixes

---
+++ src/main.js
@@ -0,0 +1,28 @@
+//UUID
+function s4() {
+	return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+}
+
+function guid() {
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
+
+// Outer HTML
+(function($) {
+	$.fn.outerHtml = function() {
+		if (this.length == 0)
+			return false;
+		var elem = this[0], name = elem.tagName.toLowerCase();
+		if (elem.outerHTML)
+			return elem.outerHTML;
+		var attrs = $.map(elem.attributes, function(i) {
+			return i.name + '="' + i.value + '"';
+		});
+		return "<" + name + (attrs.length > 0 ? " " + attrs.join(" ") : "") + ">" + elem.innerHTML + "</" + name + ">";
+	};
+})(jQuery);
+
+
+
