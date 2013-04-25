cf7e311fd9cfacfd6d38ddb9d2727cac405a36f9
Krystian Kuczek <krystian@hexmedia.pl>
2013-04-25 20:13:08 +0200

    Shorter table header template

--- src/table.js
+++ src/table.js
@@ -15,12 +15,10 @@
 	};
 
 	ko.bootstrap.te.addTemplate("kb_table_header", "\
-		<thead><tr>\
-			<!-- ko foreach: columns -->\
+		<thead><tr data-bind=\"foreach: columns\">\
 				<th data-bind=\"attr: {'class': name}\">\
 					<div data-bind=\"html: display\"></div>\
 				</th>\
-			<!-- /ko -->\
 		</tr></thead>");
 	ko.bootstrap.te.addTemplate("kb_table_item", "<tbody data-bind=\"foreach: items\">\
 			<tr data-bind=\"foreach: $parent.columns\">\
